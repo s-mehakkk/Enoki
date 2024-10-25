6003d56d1066c58293f645d0234ffa55f5df7f43
s-mehakkk <75841992+s-mehakkk@users.noreply.github.com>
2024-10-25 23:13:46 +0530

    Add components

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('footer-placeholder').innerHTML='<object type="text/html" data="./components/footer/footer.html"></object>';
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('try-placeholder').innerHTML='<object type="text/html" data="./components/try/try.html"></object>';
+});
