bd0911ece24b9a56134b25441e3b2e774ad030b7
s-mehakkk <75841992+s-mehakkk@users.noreply.github.com>
2024-10-26 22:51:01 +0530

    Edit homepage, footer navigation, update contact information

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('try-placeholder').innerHTML='<object type="text/html" data="./components/try/try.html"></object>';
 });
+
+$(function(){
+    $("#includedHeader").load("./components/header/header.html"); 
+});
+$(function(){
+    $("#includedFooter").load("./components/footer/footer.html"); 
+});
+
+window.onload = function() {
+    var headerTwo = document.getElementById('headerTwo');
+    var headerOne = document.getElementById('headerOne');
+    
+    // Set the width of headerOne to be the same as the computed width of headerTwo
+    headerOne.style.width = headerTwo.offsetWidth + 'px';
+};
